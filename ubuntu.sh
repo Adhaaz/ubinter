9b5faed5d6c6f0458d9496f4eea03d3ec0d57636
Daa <75169859+Adhaaz@users.noreply.github.com>
2024-11-30 14:54:15 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,143 +1,138 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-time1="$( date +"%r" )"
-
-install1 () {
-    directory=ubuntu-fs
-    UBUNTU_VERSION='22.04'  # Changed to Jammy Jellyfish
-    if [ -d "$directory" ]; then
-        first=1
-        printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;227m[WARNING]:\e[0m \x1b[38;5;87m Skipping the download and the extraction\n"
-    elif [ -z "$(command -v proot)" ]; then
-        printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;203m[ERROR]:\e[0m \x1b[38;5;87m Please install proot.\n"
-        printf "\e[0m"
-        exit 1
-    elif [ -z "$(command -v wget)" ]; then
-        printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;203m[ERROR]:\e[0m \x1b[38;5;87m Please install wget.\n"
-        printf "\e[0m"
-        exit 1
-    fi
-
-    if [ "$first" != 1 ]; then
-        if [ -f "ubuntu.tar.gz" ]; then
-            rm -rf ubuntu.tar.gz
-        fi
-
-        if [ ! -f "ubuntu.tar.gz" ]; then
-            printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Downloading the ubuntu rootfs, please wait...\n"
-            ARCHITECTURE=$(dpkg --print-architecture)
-            case "$ARCHITECTURE" in
-                aarch64) ARCHITECTURE=arm64;;
-                arm) ARCHITECTURE=armhf;;
-                amd64|x86_64) ARCHITECTURE=amd64;;
-                *)
-                    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;203m[ERROR]:\e[0m \x1b[38;5;87m Unknown architecture: $ARCHITECTURE"
-                    exit 1
-                    ;;
-            esac
-
-            wget https://cdimage.ubuntu.com/ubuntu-base/releases/${UBUNTU_VERSION}/release/ubuntu-base-${UBUNTU_VERSION}-base-${ARCHITECTURE}.tar.gz -q -O ubuntu.tar.gz 
-            printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Download complete!\n"
-        fi
-
-        cur=`pwd`
-        mkdir -p $directory
-        cd $directory
-        printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Decompressing the ubuntu rootfs, please wait...\n"
-        proot --link2symlink tar -zxf $cur/ubuntu.tar.gz --exclude='dev' ||:
-        printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m The ubuntu rootfs has been successfully decompressed!\n"
-        printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Fixing the resolv.conf, so that you have access to the internet\n"
-        
-        # Updated nameserver to Google's DNS
-        printf "nameserver 8.8.8.8\nnameserver 8.8.4.4\n" > etc/resolv.conf
-        
-        stubs=()
-        stubs+=('usr/bin/groups')
-        for f in ${stubs[@]}; do
-            printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Writing stubs, please wait...\n"
-            echo -e "#!/bin/sh\nexit" > "$f"
-        done
-        printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Successfully wrote stubs!\n"
-        cd $cur
-    fi
-
-    mkdir -p ubuntu-binds
-    bin=startubuntu.sh
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Creating the start script, please wait...\n"
-    
-    # Script content for starting Ubuntu
-    cat > $bin <<• EOM
-#!/bin/bash
-cd \$(dirname \$0)
-## unset LD_PRELOAD in case termux-exec is installed
-unset LD_PRELOAD
-command="proot"
-# Uncomment this line if you are having FATAL: kernel too old message.
-# command+=" -k 4.14.81"
-command+=" --link2symlink"
-command+=" -0"
-command+=" -r $directory"
-if [ -n "\$(ls -A ubuntu-binds)" ]; then
-    for f in ubuntu-binds/*; do
-      . \$f
-    done
+# Check for root access
+if [ "$(id -u)" == "0" ]; then
+   echo "This script should not be run as root. Exiting."
+   exit 1
 fi
-command+=" -b /dev"
-command+=" -b /proc"
-command+=" -b /sys"
-command+=" -b ubuntu-fs/tmp:/dev/shm"
-command+=" -b /data/data/com.termux"
-command+=" -b /:/host-rootfs"
-command+=" -b /sdcard"
-command+=" -b /storage"
-command+=" -b /mnt"
-command+=" -w /root"
-command+=" /usr/bin/env -i"
-command+=" HOME=/root"
-command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
-command+=" TERM=\$TERM"
-command+=" LANG=C.UTF-8"
-command+=" /bin/bash --login"
-com="\$@"
-if [ -z "\$1" ]; then
-    exec \$command
-else
-    \$command -c "\$com"
-fi
-EOM
-    
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m The start script has been successfully created!\n"
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Fixing shebang of startubuntu.sh, please wait...\n"
-    
-    termux-fix-shebang $bin
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Successfully fixed shebang of startubuntu.sh!\n"
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Making startubuntu.sh executable, please wait...\n"
-    chmod +x $bin
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Successfully made startubuntu.sh executable\n"
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Cleaning up, please wait...\n"
-    
-    rm ubuntu.tar.gz -rf
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m Successfully cleaned up!\n"
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;83m[Installer thread/INFO]:\e[0m \x1b[38;5;87m The installation has been completed! You can now launch Ubuntu with ./startubuntu.sh\n"
-    printf "\e[0m"
+
+# Declare variables
+UBUNTU_URL="https://cdimage.ubuntu.com/ubuntu-base/jammy/ubuntu-base-22.04.5-base-arm64.tar.gz"
+UBUNTU_TAR="ubuntu-base-jammy-arm64.tar.gz"
+DEST_DIR="$HOME/ubuntu-jammy"
+USERNAME="daffa"
+PASSWORD="Daffa30"
+MIRRORED_URL="http://mirrors.kernel.org/ubuntu/dists/jammy/main/binary-arm64/Packages.gz"
+PKG_LIST_FILE="/tmp/packages_list.txt"
+PROOT_TMP="/tmp/proot_tmp"
+
+# Function to ask for confirmation
+confirm() {
+    read -r -p "Do you want to proceed? [Y/n] " response
+    case "$response" in
+        [nN][oO]|[nN])
+            echo "Installation cancelled."
+            exit 0
+            ;;
+        *)
+            ;;
+    esac
 }
 
-if [ "$1" = "-y" ]; then
-    install1
-elif [ "$1" = "" ]; then
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;127m[QUESTION]:\e[0m \x1b[38;5;87m Do you want to install ubuntu-in-termux? [Y/n] "
-    
-    read cmd1
-    if [ "$cmd1" = "y" ]; then
-        install1
-    elif [ "$cmd1" = "Y" ]; then
-        install1
-    else
-        printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;203m[ERROR]:\e[0m \x1b[38;5;87m Installation aborted.\n"
-        printf "\e[0m"
-        exit
+# Check if required tools are installed
+check_tool() {
+    if ! command -v "$1" &> /dev/null; then
+        echo "$1 is required but not installed. Installing $1..."
+        pkg install "$1" -y
     fi
-else
-    printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;203m[ERROR]:\e[0m \x1b[38;5;87m Installation aborted.\n"
-    printf "\e[0m"
+}
+
+# Install required tools
+for tool in proot wget tar; do
+    check_tool $tool
+done
+
+# Create necessary directories
+mkdir -p "$DEST_DIR" "$PROOT_TMP"
+
+# Download Ubuntu base image
+echo "Downloading Ubuntu Jammy base system..."
+if ! wget -O "$DEST_DIR/$UBUNTU_TAR" "$UBUNTU_URL"; then
+    echo "Failed to download the Ubuntu base system. Please check your internet connection."
+    exit 1
 fi
+confirm
+
+# Extract the tarball and handle possible errors
+echo "Extracting Ubuntu image..."
+tar --warning=no-unknown-keyword -xzf "$DEST_DIR/$UBUNTU_TAR" -C "$DEST_DIR"
+if [ $? -ne 0 ]; then
+    echo "Error during extraction. Attempting to fix common issues..."
+    tar --warning=no-unknown-keyword --ignore-zeros -xzf "$DEST_DIR/$UBUNTU_TAR" -C "$DEST_DIR"
+    if [ $? -ne 0 ]; then
+        echo "Failed to extract tarball even after trying to fix errors. Exiting."
+        exit 1
+    fi
+fi
+
+# Link proot to symlink
+proot --link2symlink "$DEST_DIR"
+
+# Download and extract package lists for package detection
+echo "Downloading package list for better package management..."
+wget -O - "$MIRRORED_URL" | gunzip -c > "$PKG_LIST_FILE"
+
+# Start the chroot session with additional configurations
+echo "Setting up the chroot environment..."
+proot -0 -r "$DEST_DIR" -b /dev -b /proc -b /sys -b /system -b /data/data/com.termux/files/usr/bin:/bin -w / -0 \
+    /bin/sh -c "echo 'root:toor' | chpasswd; \
+    useradd -m -s /bin/bash $USERNAME; \
+    echo '$USERNAME:$PASSWORD' | chpasswd; \
+    dpkg --print-architecture; \
+    cp /etc/apt/sources.list /etc/apt/sources.list.bak; \
+    echo 'deb http://mirrors.kernel.org/ubuntu/ jammy main universe' > /etc/apt/sources.list; \
+    apt-get update; \
+    apt-get upgrade -y; \
+    apt-get install -y sudo tar wget; \
+    echo '$USERNAME ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers; \
+    echo 'Acquire::http { Proxy \"http://127.0.0.1:8080/\"; };' > /etc/apt/apt.conf.d/99proxy; \
+    su - $USERNAME -c 'echo \"export PS1=\\\"\$USER@jammy:\\\\W\\\\$ \\\"\" >> ~/.bashrc'; \
+    su - $USERNAME -c 'echo \"source /etc/profile\" >> ~/.bashrc'; \
+    su - $USERNAME"
+
+echo "Ubuntu Jammy installation complete!"
+echo "You can now start your Ubuntu environment by running 'proot -0 -r $DEST_DIR -b /dev -b /proc -b /sys -b /system -b /data/data/com.termux/files/usr/bin:/bin -w / /bin/bash -l'"
+
+# Script to check for updates
+echo "Creating a script to check for updates..."
+cat << 'EOF' > "$DEST_DIR/check_update.sh"
+#!/bin/bash
+apt update
+apt upgrade -y
+echo "System updated to the latest packages."
+EOF
+chmod +x "$DEST_DIR/check_update.sh"
+
+# Script to run commands inside chroot
+echo "Creating a script to run commands inside chroot..."
+cat << 'EOF' > "$DEST_DIR/run.sh"
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: $0 <command>"
+    exit 1
+fi
+proot -0 -r $HOME/ubuntu-jammy -b /dev -b /proc -b /sys -b /system -b /data/data/com.termux/files/usr/bin:/bin -w / /bin/sh -c "su - daffa -c \"$@\""
+EOF
+chmod +x "$DEST_DIR/run.sh"
+
+# Script to cleanup
+echo "Creating a cleanup script..."
+cat << 'EOF' > "$DEST_DIR/cleanup.sh"
+#!/bin/bash
+rm -rf $HOME/ubuntu-jammy
+rm $HOME/check_update.sh $HOME/run.sh $HOME/cleanup.sh
+echo "Ubuntu Jammy environment has been removed."
+EOF
+chmod +x "$DEST_DIR/cleanup.sh"
+
+# Create symbolic links in Termux home for easy access
+ln -s "$DEST_DIR/check_update.sh" "$HOME/check_update"
+ln -s "$DEST_DIR/run.sh" "$HOME/run"
+ln -s "$DEST_DIR/cleanup.sh" "$HOME/cleanup"
+
+echo "Additional utility scripts have been created:"
+echo "  - Run 'check_update' to update Ubuntu packages"
+echo "  - Run 'run <command>' to execute commands inside the Ubuntu environment"
+echo "  - Run 'cleanup' to remove the Ubuntu environment"
+
+echo "Installation and setup of Ubuntu Jammy completed successfully!"
